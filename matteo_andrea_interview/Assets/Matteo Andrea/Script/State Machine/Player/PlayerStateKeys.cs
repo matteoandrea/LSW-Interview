e632d0d0a5217121505e2ddce3383457409f2dc2
Matteo Andrea <61761339+matteoandrea@users.noreply.github.com>
2021-08-15 22:27:45 -0300

    feat: Add player move sound

--- matteo_andrea_interview/Assets/Matteo Andrea/Script/State Machine/Player/PlayerStateKeys.cs
+++ matteo_andrea_interview/Assets/Matteo Andrea/Script/State Machine/Player/PlayerStateKeys.cs
@@ -9,6 +9,8 @@ public class PlayerStateKeys : StateMachineKeys
     public readonly string navAgent = "NavAgent";
     public readonly string interact = "Interact";
 
+    public readonly string audioSource = "AudioSource";
+
 
     public override void CreatKeys(StateMachineData stateMachineData)
     {
@@ -16,10 +18,12 @@ public class PlayerStateKeys : StateMachineKeys
         var animations = stateMachineData.GetComponent<PlayerAnimations>();
         var navAgent = stateMachineData.GetComponent<NavMeshAgent>();
         var interact = stateMachineData.GetComponent<PlayerController>().playerInteract;
+        var audioSource = stateMachineData.GetComponent<AudioSource>();
 
         stateMachineData.CacheData(this.animations, animations);
         stateMachineData.CacheData(this.playerTransform, playerTransform);
         stateMachineData.CacheData(this.navAgent, navAgent);
         stateMachineData.CacheData(this.interact, interact);
+        stateMachineData.CacheData(this.audioSource, audioSource);
     }
 }
